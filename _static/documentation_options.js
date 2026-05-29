62cfc4080bf8ab907043f654577164324aeacc9e
jondo <jondo@users.noreply.github.com>
2026-05-29 12:36:34 +0000

    [ci skip] Merge pull request #12 from m3dm-jku/validate_data
    Fix _validate_data deprecation warning 3728775819ac76d0bb622afd129bd409b1f13b71

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.3.dev11+gb6409ac67',
+    VERSION: '0.1.3.dev13+g372877581',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
