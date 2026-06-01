5dc3f62a1f5599af07acf77545fc40b16f157c63
jondo <jondo@users.noreply.github.com>
2026-06-01 12:54:16 +0000

    [ci skip] pixi update f19eb0dcab80cc8770651e1cbc9a757d294fc66d

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.3.dev12+g7141a8df1',
+    VERSION: '0.1.3.dev13+gf19eb0dca',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
