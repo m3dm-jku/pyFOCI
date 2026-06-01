1b0132ea302cc392e3f65a52a8bd72f6041ad3da
jondo <jondo@users.noreply.github.com>
2026-06-01 13:43:45 +0000

    [ci skip] Update the license annotation format
    This fixes
    "SetuptoolsDeprecationWarning: License classifiers are deprecated."
    during "pixi run build". 66d97cbc3c4f6a50a7ebc79d79beb61a9ffaf889

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.3.dev13+gf19eb0dca',
+    VERSION: '0.1.3.dev14+g66d97cbc3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
