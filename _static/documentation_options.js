b6a26d24a084298e6e84285674d5c59d170dc7b2
jondo <jondo@users.noreply.github.com>
2026-06-01 12:20:16 +0000

    [ci skip] Get a Codecov result posting only if coverage drops
    See
    https://docs.codecov.com/docs/pull-request-comments#requiring-changes
    https://docs.codecov.com/docs/codecov-yaml 7141a8df1ca6ecde49375f1cba8de0e5aaf7267e

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.3.dev11+g8d9166595',
+    VERSION: '0.1.3.dev12+g7141a8df1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
