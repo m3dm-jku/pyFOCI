b874ca8c76dc9e138c5f7d38cea68e66b8d6e7c9
jondo <jondo@users.noreply.github.com>
2026-06-01 10:53:47 +0000

    [ci skip] Update to scikit-learn 1.8 8d9166595a54f98209128f5e600baf46af3ddd63

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.3.dev10+g8b3b9f833',
+    VERSION: '0.1.3.dev11+g8d9166595',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
