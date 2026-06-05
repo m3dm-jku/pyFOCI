334a110dae4480972b6d80185b74bf62f2105810
jondo <jondo@users.noreply.github.com>
2026-06-05 11:09:20 +0000

    [ci skip] Name the example file more specificly c2db0a00e7b08f7411706127ef82354bb9474872

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.3.dev23+gd8d110a83',
+    VERSION: '0.1.3.dev27+gc2db0a00e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
